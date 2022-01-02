751d31fbb252a34542fa35fa9ff6bb3dde907637
Milton Galindo <milton.elsalvador26@gmail.com>
2022-01-02 15:00:24 -0600

    Seccion numero 4 del curso terminada, se agrego 03-counter-app.

---
+++ 03-counter-app/src/PrimeraApp.js
@@ -0,0 +1,38 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+
+//asi es como creamos props, como argumentos recibidos en PrimeraApp, que es nuestro componente
+const PrimeraApp = ( 
+    {
+        saludo,
+        subtitulo
+    } 
+     ) => {
+
+    
+    
+    console.log(saludo)
+
+
+    return (
+        <>
+
+            <h1>{ saludo }</h1>
+            <p> { subtitulo } </p>
+        
+        </>
+
+        ); 
+
+} 
+
+PrimeraApp.propTypes = {
+    saludo: PropTypes.string.isRequired,
+}
+
+PrimeraApp.defaultProps = {
+    subtitulo: 'soy un subtitulo por defecto 2'
+}
+
+export default PrimeraApp;
